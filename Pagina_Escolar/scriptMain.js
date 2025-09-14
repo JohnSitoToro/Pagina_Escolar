37bbe4c1de2df0dfbfbb8356a40a7fedf239072c
JohnSitoToro <johnsito.toro13@gmail.com>
2025-09-13 21:35:05 -0500

    resolve

--- Pagina_Escolar/scriptMain.js
+++
@@ -1,7 +0,0 @@
-function abrirLightbox(id) {
-  document.getElementById(id).classList.add("activo");
-}
-function cerrarLightbox(id) {
-  document.getElementById(id).classList.remove("activo");
-}
-
